470dd0172069d69061ce7e9a1bf50d3f43d571cc
Nadine Siccion <nv.siccion@gmail.com>
2024-02-27 14:46:21 +0800

    Add Opacity increase on click functionality
    When clicking on a pixel in board, opacity for
    black background color increased. Click 10 times
    for max opacity per pixel.

--- script.js
+++ script.js
@@ -10,17 +10,33 @@ let pixelSide = Math.floor(((BOARDSIDE)/boardW)-2);
 console.log('pixelSide is ' + pixelSide)
 
 // generate pixel for board
-let pixelArray = [];
 let target;
 for (let i = 1; i < boardSize+1; i += 1) {
     target = document.createElement('div');
     target.classList.add('pixel');
     
-    console.log('target is an instance of HTMLDivElement');
     target.style.width = pixelSide + 'px';
     target.style.height = pixelSide + 'px';
+    target.style.backgroundColor = 'rgba(255,255,255, 0.0)'
     target.textContent = i
     boardDiv.appendChild(target);
 }
 
+// add opacity on click
+boardDiv.addEventListener('click', (event) => {
+    event.stopPropagation();
+    let target = event.target;
+    // console.log(target.style.backgroundColor);
+    let rgbaArray = (target.style.backgroundColor.split(','))
+    if (rgbaArray[3] == null) {
+        console.log('bgcolor set to max opacity')
+    } else if (Number(rgbaArray[3].replace(")", "")) == 0) {
+        target.style.backgroundColor = `rgba(0, 0, 0, ${0.1})`
+        console.log('bgcolor set')
+    } else {
+        target.style.backgroundColor = `rgba(0, 0, 0, ${Number(rgbaArray[3].replace(")", ""))+0.1})`
+        console.log('bgcolor opacity decreased')
+    }
+    console.log(target.style.backgroundColor)
+})
 
