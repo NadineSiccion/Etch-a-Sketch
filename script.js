361b1383ee9988843d99cdf2b559a40013f5c17c
Nadine Siccion <nv.siccion@gmail.com>
2024-02-27 13:38:18 +0800

    Establish pixel generation logic for squares
    Fixed the math for generating pixels on a square
    board.

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
-const boardDiv = document.querySelector('div.board');
+// start with a 16x16 functionality
+const boardDiv = document.querySelector('div#board');
+const BOARDSIDE = 960;
+
+// Establish board size
+let boardW = 20;
+let boardH = 20;
+let boardSize = boardW*boardH;
+let pixelSide = Math.floor(((BOARDSIDE)/boardW)-2);
+console.log('pixelSide is ' + pixelSide)
+
+// generate pixel for board
+let pixelArray = [];
+let target;
+for (let i = 1; i < boardSize+1; i += 1) {
+    target = document.createElement('div');
+    target.classList.add('pixel');
+    
+    console.log('target is an instance of HTMLDivElement');
+    target.style.width = pixelSide + 'px';
+    target.style.height = pixelSide + 'px';
+    target.textContent = i
+    boardDiv.appendChild(target);
+}
+
+
